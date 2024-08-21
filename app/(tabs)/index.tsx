097aa9e2d8c78dd6543dd8d636cfb67ecb499ef4
Nicolás Aldao <nicolasaldao@live.com>
2024-08-21 20:56:05 -0300

    work in progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,26 +3,36 @@ import { ScrollView, Text, View } from 'react-native';
 import { Deck } from '@/components/atoms/Deck';
 import { PROFILES_DATA } from '@/constants';
 import { Card, Image } from '@rneui/base';
+import { ThemedText } from '@/components/atoms/ThemedText';
 
 export default function HomeScreen() {
   const renderCard = (item) => {
     return (
-      <Card containerStyle={{ padding: 0, margin: 0, alignItems: "center", borderRadius: 20 }}>
+      <Card containerStyle={{
+        padding: 0, margin: 0, alignItems: "center", borderRadius: 20, width: 300
+      }}
+        wrapperStyle={{}}>
         <Image
-          style={{ width: 300, height: 400 }}
-          resizeMode="contain"
+          style={{ width: 300, height: 520 }}
+          resizeMode="cover"
           source={{ uri: item.photo }}
         />
-        <Card.Title>{item.name}</Card.Title>
+        <Card.Title style={{ marginBottom: 0 }}>{item.name}</Card.Title>
       </Card>
     )
   }
 
+  const renderNoMoreCards = () => {
+    return <ThemedText style={{ color: 'red' }}>No more cards!!</ThemedText>
+  }
+
   return (
-    <ScrollView>
-      <View style={{ flex: 1 }}>
-        <Deck data={PROFILES_DATA} renderCard={renderCard} />
-      </View>
-    </ScrollView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Deck data={PROFILES_DATA} renderCard={renderCard}
+        onSwipeLeft={() => console.log('Something was swipped')}
+        onSwipeRight={() => console.log('Something was swipped')}
+        renderNoMoreCards={renderNoMoreCards}
+      />
+    </View>
   );
 }
--- components/atoms/Deck/index.tsx
+++ components/atoms/Deck/index.tsx
@@ -1,14 +1,27 @@
-import { FC } from "react";
-import { Animated, PanResponder, View } from "react-native"
+import { FC, ReactNode, useState } from "react";
+import { Animated, Dimensions, PanResponder, View } from "react-native"
 
 import { PROFILES_DATA } from "@/constants"
 
 type Props = {
     data: any;
     renderCard: (item: any) => any;
+    onSwipeLeft?: (item: any) => void;
+    onSwipeRight?: (item: any) => void;
+    renderNoMoreCards: () => ReactNode;
 };
 
-export const Deck: FC<Props> = ({ data, renderCard }) => {
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.25;
+
+export const Deck: FC<Props> = ({
+    data,
+    renderCard,
+    onSwipeLeft = (item) => { },
+    onSwipeRight = (item) => { },
+    renderNoMoreCards,
+}) => {
+    const [cardIndex, setCardIndex] = useState(1)
     const position = new Animated.ValueXY();
 
     const panResponder = PanResponder.create({
@@ -16,18 +29,51 @@ export const Deck: FC<Props> = ({ data, renderCard }) => {
         onPanResponderMove: (event, gesture) => {
             position.setValue({ x: gesture.dx, y: gesture.dy })
         }, // when user drag finger in screen
-        onPanResponderRelease: () => { console.log() }, // when user remove finger from screen
+        onPanResponderRelease: (event, gesture) => {
+            if (gesture.dx > SWIPE_THRESHOLD) {
+                forceSwipe('right');
+            } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                forceSwipe('left');
+            } else {
+                resetPosition();
+            }
+        }, // when user remove finger from screen
     })
+
+    const resetPosition = () => {
+        Animated.spring(position, {
+            useNativeDriver: false,
+            toValue: { x: 0, y: 0 },
+        }).start();
+    }
+
+    const forceSwipe = (direction: string) => {
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(position, {
+            useNativeDriver: false,
+            toValue: { x, y: 0 },
+            duration: 250
+        }).start(() => onSwipeComplete(direction));
+    }
+
+    const onSwipeComplete = (direction: string) => {
+        // const { onSwipeLeft, onSwipeRight } = 
+        const item = data[cardIndex];
+
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        position.setValue({ x: 0, y: 0 })
+        setCardIndex((prevState) => prevState + 1);
+    }
 
     const getCardStyle = () => {
         const rotate = position.x.interpolate({
-            // inputRange and outputRange are related, we set the maximum number of pixels to move to the left or right to be 500.
-            // that will move to the left or to the right will be 500, if the maximum stops are reached,
+            // inputRange and outputRange are related, we set the maximum number of pixels to move to the left or right to be SCREEN_WIDTH.
+            // that will move to the left or to the right will be SCREEN_WIDTH, if the maximum stops are reached,
             // of each end, we will rotate 120 degrees maximum, on the other hand if we don't reach the maximum,
             // but if we go out of the center we will rotate 120 degrees maximum.
             // but if we go out of the center which is 0, we will rotate the 
             // percentage proportional to that range
-            inputRange: [-500, 0, 500],
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
             outputRange: ['-120deg', '0deg', '120deg']
         });
 
@@ -37,22 +83,17 @@ export const Deck: FC<Props> = ({ data, renderCard }) => {
         }
     }
 
-    return (
-        <View>
-            <>
-                {PROFILES_DATA.map((item) => {
-                    if (item.id === 1) {
-                        return <Animated.View key={item.id}
-                            style={getCardStyle()}
-                            {...panResponder.panHandlers}>
-                            {renderCard(item)}
-                        </Animated.View>
-                    }
-                    return <View key={item.id}>
-                        {renderCard(item)}
-                    </View>
-                })}
-            </>
-        </View>
-    )
+    if (PROFILES_DATA.length < cardIndex) return renderNoMoreCards();
+
+    return PROFILES_DATA.map((item, i) => {
+        if (i + 1 < cardIndex) return null;
+        if (item.id === cardIndex) {
+            return <Animated.View key={item.id}
+                style={getCardStyle()}
+                {...panResponder.panHandlers}
+            >
+                {renderCard(item)}
+            </Animated.View>
+        }
+    })
 }

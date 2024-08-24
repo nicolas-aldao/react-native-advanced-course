3db973095c3cde43dca01482dfb1e428bce6570b
Nicolás Aldao <nicolasaldao@live.com>
2024-08-24 20:49:16 -0300

    twilio integration

--- firebase-rn-advanced/functions/src/index.ts
+++ firebase-rn-advanced/functions/src/index.ts
@@ -3,8 +3,13 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 const createUser = require('./create_user');
+const requestOTP = require('./request_otp');
+const verifyOTP = require('./verify_otp');
 
 const EMULATOR_HOST_AUTH = 'localhost:9099';
+
+// run the firebase server  
+// firebase emulators:start
 
 admin.initializeApp();
 
@@ -22,3 +27,5 @@ if (process.env.FUNCTIONS_EMULATOR) {
 }
 
 exports.createUser = functions.https.onRequest(createUser);
+exports.requestOTP = functions.https.onRequest(requestOTP);
+exports.verifyOTP = functions.https.onRequest(verifyOTP);

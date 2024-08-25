0eb0af6422acc4fdcdde1560d7d881f352b562d6
Nicolás Aldao <nicolasaldao@live.com>
2024-08-24 22:11:33 -0300

    JobFinder project added

---
+++ JobFinder/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hola!</Text>
+    </View>
+  );
+}
+
+export default App;
